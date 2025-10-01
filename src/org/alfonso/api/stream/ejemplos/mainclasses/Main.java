1c843ef0fa4330838d8b4511bdced282d035695a
ladri <ladrin@gmail.com>
2025-10-01 12:46:23 +0200

    wip

---
+++ src/org/alfonso/api/stream/ejemplos/mainclasses/Main.java
@@ -0,0 +1,28 @@
+package org.alfonso.api.stream.ejemplos.mainclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<String> listaDeNombres = new ArrayList<>();
+        listaDeNombres.add("PAQUITO MASITO");
+        listaDeNombres.add("TOMASO PERALES");
+        listaDeNombres.add("TOMASA TRICOLA");
+        listaDeNombres.add("MELONCIO CABRERAS");
+        listaDeNombres.add("EUSTAQUIO CABRERAS");
+        listaDeNombres.add("MENTECATO PALURDO");
+        listaDeNombres.add("TOMASON CABEAZON");
+
+        Stream<Usuario> streamDeUsuarios = listaDeNombres.stream()
+                .map(it ->  {return  new Usuario(it.split(" ")[0], it.split(" ")[1],13,"654321K");})
+                .peek(it-> System.out.println(it));
+
+        List<Usuario> listaDeUsuarios = streamDeUsuarios.collect(Collectors.toList());
+        listaDeUsuarios.forEach(it-> System.out.println(it));
+    }
+}
