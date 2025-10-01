8e47034ac4739026fe2596bba28470f1d644d2ef
ladri <ladrin@gmail.com>
2025-10-01 17:03:23 +0200

    wip

---
+++ src/Test1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        List<String> test = new ArrayList<>();
+        test.add("Vicentin");
+        test.add("Periquin");
+        test.add("Camelas");
+
+        test.stream().map(String::toUpperCase);
+        Stream<String>stream = test.stream();
+
+
+        test.forEach(a-> System.out.println(a));
+        test.stream().forEach(a-> System.out.println(a));
+
+        Stream<String> nombres = Stream.of("Vicentico", "Moniato", "Camelas", "Bleras");
+        nombres.forEach(e -> System.out.println(e));
+
+        String[] array = {"Manolin", "Eustaquio", "Marcelo", "Benito"};
+        Stream<String> nombres2 = Arrays.stream(array);
+        nombres2.forEach(e -> System.out.println(e));
+
+        Arrays.stream(array).forEach(a-> System.out.println(a));
+
+        Stream<String> nombres3 = Stream.<String>builder().add("Eustaquio").add("Meloncio").add("Moniato").add("Camelas").build();
+        nombres3.forEach(a-> System.out.println(a));
+    }
+}
