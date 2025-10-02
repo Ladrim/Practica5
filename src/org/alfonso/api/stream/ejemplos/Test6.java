567ffdc19e048c5f4117f7527e30268c2481c9e9
ladri <ladrin@gmail.com>
2025-10-02 12:12:58 +0200

    wip

---
+++ src/org/alfonso/api/stream/ejemplos/Test6.java
@@ -0,0 +1,32 @@
+package org.alfonso.api.stream.ejemplos;
+
+import org.alfonso.api.stream.ejemplos.mainclasses.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test6
+{
+    public static void main(String[] args) throws Exception
+    {
+        List<String> listaDeNombres = new ArrayList<>();
+        listaDeNombres.add("TOMASITO PERALES");
+        listaDeNombres.add("MELONCIO LIMONES");
+        listaDeNombres.add("EUSTAQUIO MENTECATO");
+        listaDeNombres.add("EUSTAQUIO MACUTO");
+
+        boolean existe = listaDeNombres.stream()
+                .map(it ->  crearUsuario(it))
+                .anyMatch(it -> it.getId().equals(2));
+
+        System.out.println(existe);
+    }
+
+    private static Usuario crearUsuario(String it)
+    {
+        String[] valor = it.split("");
+        String nombre = valor[0];
+        String apellido = valor[1];
+        return new Usuario(nombre, apellido, null, null);
+    }
+}
