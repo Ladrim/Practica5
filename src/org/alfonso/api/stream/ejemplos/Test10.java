db94922502f4457f1c9d628e2fe62d0377ff89c8
ladri <ladrin@gmail.com>
2025-10-06 11:50:00 +0200

    wip

---
+++ src/org/alfonso/api/stream/ejemplos/Test10.java
@@ -0,0 +1,26 @@
+package org.alfonso.api.stream.ejemplos;
+
+import org.alfonso.api.stream.ejemplos.mainclasses.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test10
+{
+    public static void main(String[] args) throws Exception
+    {
+        List<String> listaDeNombres = new ArrayList<>();
+        listaDeNombres.add("TOMASITO PERALES");
+        listaDeNombres.add("MELONCIO LIMONES");
+        listaDeNombres.add("EUSTAQUIO MENTECATO");
+        listaDeNombres.add("EUSTAQUIO MACUTO");
+
+        Usuario usuario = listaDeNombres.stream()
+                .map(it ->  {return  new Usuario(it.split(" ")[0], it.split(" ")[1],null,null);})
+                .filter(it-> it.getId().equals(1))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ERROR"));
+
+        System.out.println(usuario);
+    }
+}
