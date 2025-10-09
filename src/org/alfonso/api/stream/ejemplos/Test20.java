8dbd9364b791847a90df1c70eecbb1226fdf3cba
ladri <ladrin@gmail.com>
2025-10-09 17:05:45 +0200

    wip

---
+++ src/org/alfonso/api/stream/ejemplos/Test20.java
@@ -0,0 +1,49 @@
+package org.alfonso.api.stream.ejemplos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Test20
+{
+    public static void main(String[] args)
+    {
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Trolero");
+        nombres.add("Donkey Kong");
+        nombres.add("Calculin");
+        nombres.add("Peponcio");
+        nombres.add("Regulin");
+        nombres.add("Tomasito");
+
+
+        for(String nombre : nombres)
+        {
+            System.out.println(nombre);
+        }
+        System.out.println();
+
+        nombres.forEach(nombre-> System.out.println(nombre + "\n"));
+
+        Stream<String> stream = nombres.stream()
+                .map(it -> it.toUpperCase())
+                .map(it -> it.replace("e","i"))
+                .peek(it -> System.out.println(it));
+
+
+        List<String> nombresToUpperCase = stream.collect(Collectors.toList());
+        nombresToUpperCase.forEach(nombre-> System.out.println(nombre + "\n") );
+
+
+        List<String> streamToUpperCase2 = nombres.stream()
+                .map(it -> it.toUpperCase())
+                .map(it -> it.replace("e","i"))
+                .peek(it -> System.out.println(it)).collect(Collectors.toList());
+    }
+
+    private static void printList(List<String>list)
+    {
+        list.forEach(it -> System.out.println(it));
+    }
+}
