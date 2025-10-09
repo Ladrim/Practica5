01b416708ea47888fe2cbbe259d17e8aa26febe1
ladri <ladrin@gmail.com>
2025-10-09 17:15:13 +0200

    wip

--- src/org/alfonso/api/stream/ejemplos/Test20.java
+++ src/org/alfonso/api/stream/ejemplos/Test20.java
@@ -17,6 +17,7 @@ public class Test20
         nombres.add("Regulin");
         nombres.add("Tomasito");
         nombres.add("Vicentico");
+        nombres.add("Ridiculin");
 
         for(String nombre : nombres)
         {
