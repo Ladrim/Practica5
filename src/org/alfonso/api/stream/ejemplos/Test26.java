cc9054399a88365205a28ca10ef6faa2363c9f76
ladri <ladrin@gmail.com>
2025-10-14 17:13:22 +0200

    wip

--- src/org/alfonso/api/stream/ejemplos/Test26.java
+++ src/org/alfonso/api/stream/ejemplos/Test26.java
@@ -52,4 +52,36 @@ public class Test26
                 .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
                 .findFirst();
     }
+
+    public void versionPrueba()
+    {
+        List<Persona> listaDeUsuarios = new ArrayList<>();
+        listaDeUsuarios.add(new Persona("TON","TIN",20,"12345A",1));
+        listaDeUsuarios.add(new Persona("BARTOLO","PERALES",45,"23456B",2));
+        listaDeUsuarios.add(new Persona("EUSTAQUIO","BONIATO",59,"34567C",3));
+        listaDeUsuarios.add(new Persona("CENUTRIO","MENTECATO",68,"45678D",4));
+        listaDeUsuarios.add(new Persona("BARTOLIN","CENUTRITO",20,"56789F",5));
+        listaDeUsuarios.add(new Persona("ENANIN","CENUTRITO",20,"67899J",6));
+
+        Map<Integer, Persona> mapaDePersonasPorCodigo = listaDeUsuarios.stream()
+                .collect(Collectors.toMap(it -> it.getCodigo(),it-> it));
+
+
+        Map<String,List<Persona>> mapaDePersonasPorApellido = listaDeUsuarios.stream()
+                .collect(Collectors.groupingBy(it -> it.getApellido()));
+
+        Map<String, Integer> mapaNumeroDeApellidos = mapaDePersonasPorApellido.entrySet().stream()
+                .map(it -> Map.entry(it.getKey(),it.getValue().size()))
+                .collect(Collectors.toMap(it -> it.getKey(), it-> it.getValue()));
+
+        Optional<Integer> numeroDeRepeticiones = mapaNumeroDeApellidos.entrySet().stream()
+                .sorted(Comparator.comparing((Map.Entry<String, Integer>it) -> it.getValue()).reversed())
+                .findFirst()
+                .map(it -> it.getValue());
+
+        Optional<String> nombreMasRepetido = mapaNumeroDeApellidos.entrySet().stream()
+                .sorted(Comparator.comparing((Map.Entry<String, Integer>it) -> it.getValue()).reversed())
+                .findFirst()
+                .map(it -> it.getKey());
+    }
 }
